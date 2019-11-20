8cabcddcb205bf222fbdca740edaa32546794291
unknown <garridovillazana@gmail.com>
2019-11-20 16:31:22 -0300

    card complete

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';   
-import { Card as Tarjeta, CardBody, CardTitle, CardText, CardImg, Col } from 'reactstrap';
+import { Card as Tarjeta, CardBody, CardTitle, CardText, CardImg, Col, Button, Row } from 'reactstrap';
 import { users } from '../../utils/mock-data';
 
 class Card extends Component{
@@ -11,21 +11,48 @@ class Card extends Component{
         };
     }
 
+    getAlive = a => a ? 'SI' : 'NO';
+    getAction = a => a ? 'MATAR' : 'REVIVIR';
+
+    handlerToggleAlive = (user) => {
+        const i = this.state.users.findIndex(e => e.id === user.id);
+        return (event) => {
+            const nL = this.state.users;
+            nL[i].alive = !nL[i].alive;
+
+            this.setState({
+                users: nL,
+            })
+        }
+    }
+
     render(){
         return (
             <div>
-                <Col xs="3" >
-                    <Tarjeta>
-                        <CardImg top width="20%" src="https://picsum.photos/50/50" alt="Card image cap" />
-                        <CardBody>
-                            <CardTitle>Card Title</CardTitle>
-                            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                            <CardText>
-                                <small className="text-muted">Last updated 3 mins ago</small>
-                            </CardText>
-                        </CardBody>
-                    </Tarjeta>
-                </Col>
+                <Row>
+                
+                {
+                    this.state.users.map( elm => 
+                        <Col xs="3" >
+                            <Tarjeta>
+                                <CardImg top width="20%" src="https://picsum.photos/50/50" alt="Card image cap" />
+                                <CardBody>
+                                    <CardTitle> {elm.name} </CardTitle>
+                                    <CardText> El id de {elm.name} es {elm.id} y su edad es {elm.age}</CardText>
+                                    <CardText>
+                                        <Button
+                                    block
+                                    color={elm.alive ? 'danger' : 'success'}
+                                    onClick={this.handlerToggleAlive(elm)}> {this.getAction(elm.alive)} </Button>
+                                    </CardText>
+                                </CardBody>
+                            </Tarjeta>
+                            </Col>
+                        )
+                    }
+                </Row>
+                    
+                   
             </div>
           );
     }
